c744b320d2476aef9e4d6971f5ac216a135db6ef
Chris Adams <achris333.ca@gmail.com>
2022-07-22 19:24:09 -0400

    Created a laser gameobject and added shooting functionality

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private float _LaserSpeed = 8f;
+    private float _boundaryX = 9.4f;
+    private float _boundaryY = 5.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * _LaserSpeed * Time.deltaTime);
+        
+        if (transform.position.x >= _boundaryX)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.y >= _boundaryY)
+        {
+            Destroy(gameObject);   
+        }      
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,31 +4,49 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float _movementSpeed = 3.5f;
+    private float _boundaryX = 9.4f;
+    private float _boundaryY = 5.5f;
     private Transform _player;
 
     [SerializeField]
-    private float _speed = 3.5f;
-    private float _boundaryX = 9.4f;
-    private float _boundaryY = 5.5f;
-    
+    private float _fireRate = 0.5f;
+
+    [SerializeField]
+    private GameObject laserPrefab;
+
+    [SerializeField]
+    private Transform _laserOffset;
+
+    private bool _canFire;
+    private IEnumerator laserCooldownTimer;
+
+
     // Start is called before the first frame update
     void Start()
     {
         _player = transform; 
-
         _player.position = new Vector3(0, 0, 0);
+
+        _canFire = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && _canFire)
+        {
+            Shoot();
+        }
     }
 
     void Movement()
     {
-        float horizontalInput = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
-        float verticalInput = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+        float horizontalInput = Input.GetAxis("Horizontal") * _movementSpeed * Time.deltaTime;
+        float verticalInput = Input.GetAxis("Vertical") * _movementSpeed * Time.deltaTime;
 
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
@@ -51,5 +69,18 @@ public class Player : MonoBehaviour
         {
             _player.position = new Vector3(_player.position.x, _boundaryY, 0);
         }
+    }
+
+    void Shoot()
+    {  
+        Instantiate(laserPrefab, _laserOffset.position, Quaternion.identity);
+        _canFire = false;
+        StartCoroutine(LaserCoolDownTimer());
+    }
+
+    IEnumerator LaserCoolDownTimer()
+    {
+        yield return new WaitForSeconds(_fireRate);
+        _canFire = true;
     }
 }
